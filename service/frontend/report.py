bbf125542947845fb1d3050365374f3180abc6d0
malickijaleksandr <cfif5665@gmail.com>
2024-11-17 13:15:27 +0600

    add front

---
+++ service/frontend/report.py
@@ -0,0 +1,18 @@
+import streamlit as st
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+
+async def report():
+    if 'predicted_data' not in st.session_state:
+        class_names = list(set(st.session_state['predicted_data']))
+        y_pred = st.session_state['predicted_data']
+
+        # Распределение классов
+        st.subheader("Распределение классов")
+        class_distribution = pd.DataFrame(
+            {'Class': class_names, 'Count': [list(y_pred).count(i) for i in range(len(class_names))]})
+        fig, ax = plt.subplots()
+        sns.barplot(x='Class', y='Count', data=class_distribution, ax=ax)
+        st.pyplot(fig)
---
+++ service/frontend/upload.py
@@ -0,0 +1,25 @@
+import json
+import pyarrow as pa
+import requests
+import streamlit as st
+import pandas as pd
+
+
+
+async def upload():
+    st.subheader("Загрузка данных")
+    tsv_file = st.file_uploader("Загрузите TSV файл", type=["tsv"])
+    if tsv_file is not None:
+        data = pd.read_csv(tsv_file, encoding='utf-8', header=None,  names=['index', 'date', 'cash', 'description', 'type'], sep='\t', index_col=0)
+
+        if st.button('Получить предсказание'):
+            url = f'http://localhost:8910/predict/batch'
+            data = data['description']
+            headers = {"Content-Type": "application/json"}
+            response = requests.post(url, headers=headers, data=json.dumps(list(data))).json()
+
+            data['тип платежа'] = response
+            st.session_state['predicted_data'] = response
+            st.subheader("Данные:")
+
+            st.write(data)
